9610c9353e6ca22d0a792a2755fb37a271f961ca
Naqi Bhatti <naqibhatti@gmail.com>
2024-06-24 13:41:57 +0500

    basic card setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Card from "./card"
+function App() {
+  return (
+    <Card></Card>
+  );
+}
+
+export default App
---
+++ src/card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+    return(
+        <div className="card">
+            <img alt="Profile picture" src="https://via.placeholder.com/150"></img>
+            <h2>Bro code</h2>
+            <p>Queen Mary University of London
+            </p>
+        </div>
+    );
+}
+
+export default Card
